65d8e09e4b8381f4f30dd081f0fee4516f8aaa8c
Eric Poole <thatonepoole@gmail.com>
2021-09-27 11:47:59 -0600

    making it into a raspberry pi project

--- blink.py
+++ blink.py
@@ -1,14 +1,19 @@
+import wave  
+w = wave.open('f.wav', 'r')  
+for i in range():  
+    frame = w.readframes(i)
 
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
-GPIO.setwarnings(False) # Ignore warning for now
-print('setwarnings done')
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-print('setmode')
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-print('setup')
-while True: # Run forever
- GPIO.output(8, GPIO.HIGH) # Turn on
- sleep(1) # Sleep for 1 second
- GPIO.output(8, GPIO.LOW) # Turn off
- sleep(1) # Sleep for 1 second
+
+# import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+# from time import sleep # Import the sleep function from the time module
+# GPIO.setwarnings(False) # Ignore warning for now
+# print('setwarnings done')
+# GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+# print('setmode')
+# GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+# print('setup')
+# while True: # Run forever
+#  GPIO.output(8, GPIO.HIGH) # Turn on
+#  sleep(1) # Sleep for 1 second
+#  GPIO.output(8, GPIO.LOW) # Turn off
+#  sleep(1) # Sleep for 1 second
