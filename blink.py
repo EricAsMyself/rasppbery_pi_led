0c8ae0f3b16d4a837c4c028994274ff1c34a7bdf
Eric Poole <thatonepoole@gmail.com>
2021-09-27 08:59:37 -0600

    testtrying to change error

---
+++ blink.py
@@ -0,0 +1,14 @@
+
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep # Import the sleep function from the time module
+GPIO.setwarnings(False) # Ignore warning for now
+print('setwarnings done')
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+print('setmode')
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+print('setup')
+while True: # Run forever
+ GPIO.output(8, GPIO.HIGH) # Turn on
+ sleep(1) # Sleep for 1 second
+ GPIO.output(8, GPIO.LOW) # Turn off
+ sleep(1) # Sleep for 1 second
